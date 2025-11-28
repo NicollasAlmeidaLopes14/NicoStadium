41e515e25b8103ad0efcf94abb4b22234d810a20
nicollasalmeidalopes14 <nicollas.lopes@sptech.school>
2025-11-27 21:30:31 -0300

    Criação da função que gera os confrontos entre os times aleatoriamente, criação da caixa que mostrará o confronto gerado, criação de uma máscara para que será habilitada no momento em que o usuário gerar os confrontos e criação da função que retira a máscara

--- public/js/game.js
+++ public/js/game.js
@@ -1,37 +1,102 @@
+
+const generateClashChampionsButton = document.getElementById('clash-button-champions')
+const generateClashLibertadoresButton = document.getElementById('clash-button-libertadores')
+const gameMask = document.getElementById('mask')
+const generatorBox = document.getElementById('generator-box')
+
+
 function loadTeams() {
-    fetch('/data/teams.json')
-        .then(function (response) {
+    fetch('/data/teams.json').then(function (response) {
+        if (response.ok) {
+            response.json().then(data => {
+                const championsContainer = document.getElementById('champions-team-container')
+                const libertadoresContainer = document.getElementById('libertadores-teams-container')
+
+                data[0].champions.forEach(championsTeam => {
+                    const championsTeamImage = document.createElement('img')
+                    championsTeamImage.src = championsTeam.logo
+                    championsTeamImage.alt = championsTeam.name
+                    championsTeamImage.id = championsTeam.styleId
+                    championsTeamImage.classList.add('champions-team-logo')
+
+                    championsContainer.appendChild(championsTeamImage)
+                });
+
+                data[1].libertadores.forEach(libertadoresTeam => {
+                    const libertadoresTeamImage = document.createElement('img')
+                    libertadoresTeamImage.src = libertadoresTeam.logo
+                    libertadoresTeamImage.alt = libertadoresTeam.name
+                    libertadoresTeamImage.id = libertadoresTeam.styleId
+
+                    libertadoresContainer.appendChild(libertadoresTeamImage)
+                });
+            })
+        } else {
+            console.log('Erro no fetch da função loadTeams')
+            throw new Error('Erro ao carregar arquivo JSON:' + response.status)
+        }
+    })
+}
+
+
+function generateClash(competition) {
+    const homeTeamLogo = document.getElementById('home-team-logo')
+    const visitingTeamLogo = document.getElementById('visiting-team-logo')
+    const homeTeamButton = document.getElementById('home-team-button')
+    const visitingTeamButton = document.getElementById('visiting-team-button')
+
+    const firstRandomTeam = Math.floor(Math.random() * 10)
+    const secondRandomTeam = Math.floor(Math.random() * 10)
+
+    if (competition === 'champions') {
+        fetch('/data/teams.json').then((response) => {
             if (response.ok) {
-                // console.log(response)
+                response.json().then(data => {
 
-                response.json()
-                    .then(data => {
-                        console.log(data)
+                    const homeTeam = data[0].champions[firstRandomTeam]
+                    const visitingTeam = data[0].champions[secondRandomTeam]
 
-                        const championsContainer = document.getElementById('champions-team-container')
-                        const libertadoresContainer = document.getElementById('libertadores-teams-container')
+                    homeTeamLogo.src = homeTeam.logo
+                    homeTeamButton.innerText = homeTeam.nickName
 
-                        data[0].champions.forEach(championsTeam => {
-                            const championsTeamImage = document.createElement('img')
-                            championsTeamImage.src = championsTeam.logo
-                            championsTeamImage.alt = championsTeam.name
-                            championsTeamImage.id = championsTeam.styleId
-                            championsTeamImage.classList.add('champions-team-logo')
-
-                            championsContainer.appendChild(championsTeamImage)
-                        });
-
-                        data[1].libertadores.forEach(libertadoresTeam => {
-                            const libertadoresTeamImage = document.createElement('img')
-                            libertadoresTeamImage.src = libertadoresTeam.logo
-                            libertadoresTeamImage.alt = libertadoresTeam.name
-                            libertadoresTeamImage.id = libertadoresTeam.styleId
-
-                            libertadoresContainer.appendChild(libertadoresTeamImage)
-                        });
-                    })
+                    visitingTeamLogo.src = visitingTeam.logo
+                    visitingTeamButton.innerText = visitingTeam.nickName
+                })
             } else {
-                throw new Error('Erro ao carregar arquivo JSON:' + response.status)
+                console.log("Erro no fetch da função generateClash")
+                throw new Error("Erro ao carregar arquivo JSON" + response.status)
             }
         })
+
+        gameMask.style.visibility = 'visible'
+        generatorBox.style.top = '25%'
+
+
+    } else {
+        fetch('/data/teams.json').then((response => {
+            if (response.ok) {
+                response.json().then(data => {
+                    var homeTeam = data[1].libertadores[firstRandomTeam]
+                    var visitingTeam = data[1].libertadores[secondRandomTeam]
+
+                    homeTeamLogo.src = homeTeam.logo
+                    homeTeamButton.innerText = homeTeam.name
+
+                    visitingTeamLogo.src = visitingTeam.logo
+                    visitingTeamButton.innerText = visitingTeam.name
+                })
+            } else {
+                throw new Error("Erro ao carregar arquivo JSON:" + response.status);
+
+            }
+        }))
+
+        gameMask.style.visibility = 'visible'
+        generatorBox.style.top = '25%'
+    }
+}
+
+function disappearMask() {
+    gameMask.style.visibility = 'hidden'
+    generatorBox.style.top = '-330px'
 }
